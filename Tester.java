4386324c79a08758c59d312960e46c7984390a1a
AZITechnology <165827060+AZITechnology@users.noreply.github.com>
2024-04-15 17:06:27 -0400

    CIS111B Tester GitHub
    This is a test with GitHub to understand how it works.

---
+++ Tester.java
@@ -0,0 +1,16 @@
+public class Tester
+   {
+      public static void main(String[] args)
+         {
+            int i = 0;
+            
+            System.out.println("Hello World!!");
+            
+            while (i < 100)
+               {
+                  System.out.println(i + 1);
+                  i++;
+               }
+         }
+   }
+                  
